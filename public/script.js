70b3b00594f815ace55f114f6250820d876885bb
Miguel Layos.JS <miguelpogilayos@gmail.com>
2022-07-08 01:05:02 +0800

    test 4

--- public/script.js
+++ public/script.js
@@ -2,25 +2,34 @@
 const socket = io('http://localhost:3000')
 
 //dom element variables
+const container = document.querySelector('.container')
 const userformInput = document.querySelector('.userform-input')
 const userNameInput = document.querySelector('.username-input')
 const joinScreen = document.querySelector('.join-screen')
 const chatScreen = document.querySelector('.chat-screen')
 
 userformInput.addEventListener('submit', (e) => {
-    console.log('submit');
     e.preventDefault()
     const name = userNameInput.value
-    socket.emit('name', name)
-    userNameInput.value = ''
-    joinScreen.classList.remove('active')
-    chatScreen.classList.add('active')
+    if (name.length >= 4) {
+        socket.emit('name', name)
+        userNameInput.value = ''
+        container.classList.add('chat-container')
+        joinScreen.classList.remove('active')
+        chatScreen.classList.add('active')
+    }
+    else {
+        alert('Invalid name')
+    }
 })
 
 socket.on('chatMessage', (data) => {
     appendMessage(data)
 })
 
+socket.on('users', (users) => {
+    console.log(users)
+})
 //function for appending the message
 function appendMessage(message) {
     const messageElement = document.createElement('div')
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { Console } = require('console');
+
 //modules
 const express = require('express'),
     app = express(),
@@ -5,28 +7,32 @@ const express = require('express'),
     socketIO = require('socket.io'),
     io = socketIO(server)
 
-var users = []
+var users = {};
 
 //static files
 app.use(express.static('public'))
 
+
 //websocket server
 io.on('connection', (socket) => {
-    console.log('a new user connected:', socket.id)
+
+    console.log(users)
     socket.on('inputMessage', (message) => {
         io.emit('chatMessage', message)
     })
-
     socket.on('name', (name) => {
-        console.log(name, socket.id)
-    })
+        users[socket.id] = name;
+        io.emit('users', users)
+        console.log(users)
+        socket.on('disconnecting', () => {
+            delete users[socket.id]
+            io.emit('users', users)
+            console.log(users)
+        })
 
-    socket.on('disconnect', () => {
     })
-
 })
 
-//listen to port
 server.listen(3000, () => {
     console.log('server is running on port 3000')
 })
