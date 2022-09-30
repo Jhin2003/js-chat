a4cc3ab80f29c70575cec6a790ffde5273d20a78
Miguel Layos.JS <91679722+Jhin2003@users.noreply.github.com>
2022-09-30 17:10:54 +0800

    test 6

--- public/script.js
+++ public/script.js
@@ -1,58 +1,62 @@
 //establish socket connection
-const socket = io('http://localhost:3000')
+const socket = io("http://localhost:3000");
 
 //dom element variables
-const container = document.querySelector('.container')
-const userformInput = document.querySelector('.userform-input')
-const userNameInput = document.querySelector('.username-input')
-const joinScreen = document.querySelector('.join-screen')
-const chatScreen = document.querySelector('.chat-screen')
-const onlineUsers = document.querySelector('.chat-right-side')
+const container = document.querySelector(".container");
+const userformInput = document.querySelector(".userform-input");
+const userNameInput = document.querySelector(".username-input");
+const joinScreen = document.querySelector(".join-screen");
+const chatScreen = document.querySelector(".chat-screen");
+const onlineUsers = document.querySelector(".chat-right-side");
+const chat = document.querySelector(".chat-form");
+const chatInput = document.querySelector(".chat-input");
 
-userformInput.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const name = userNameInput.value
-    if (name.length >= 4) {
-        socket.emit('name', name)
-        userNameInput.value = ''
-        container.classList.add('chat-container')
-        joinScreen.classList.remove('active')
-        chatScreen.classList.add('active')
-    }
-    else {
-        alert('name must  atleast cointain 4 characthers')
-    }
-})
+userformInput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = userNameInput.value;
+  if (name.length >= 4) {
+    socket.emit("name", name);
+    userNameInput.value = "";
+    container.classList.add("chat-container");
+    joinScreen.classList.remove("active");
+    chatScreen.classList.add("active");
+  } else {
+    alert("name must  atleast cointain 4 characthers");
+  }
+});
 
-socket.on('chatMessage', (data) => {
+chat.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = chatInput.value;
+  socket.emit("inputMessage", message);
+  chatInput.value = "";
+});
 
-})
+socket.on("chatMessage", (message) => {});
 
-socket.on('users', (users) => {
-    showOnlineUsers(users)
-})
+socket.on("users", (users) => {
+  showOnlineUsers(users);
+});
+
 //function for appending the message
 function appendMessage(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
-    console.log(messageContainer.innerText)
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+  console.log(messageContainer.innerText);
 }
 
 //function for showing online users
 function showOnlineUsers(message) {
-    //delete users that left
-    while (onlineUsers.hasChildNodes()) {
-        onlineUsers.removeChild(onlineUsers.firstChild);
-    }
-    //add users that joined
-    let onlineUsersList = Object.values(message)
-    onlineUsersList.forEach((user) => {
-        let displayOnlineUser = document.createElement('div')
-        displayOnlineUser.innerText = user
-        onlineUsers.append(displayOnlineUser)
-    });
-
-
+  //delete users that left
+  while (onlineUsers.hasChildNodes()) {
+    onlineUsers.removeChild(onlineUsers.firstChild);
+  }
+  //add users that joined
+  let onlineUsersList = Object.values(message);
+  onlineUsersList.forEach((user) => {
+    let displayOnlineUser = document.createElement("div");
+    displayOnlineUser.innerText = user;
+    onlineUsers.append(displayOnlineUser);
+  });
 }
-
--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 //modules
-const express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    socketIO = require('socket.io'),
-    io = socketIO(server)
+const express = require("express"),
+  app = express(),
+  server = require("http").createServer(app),
+  socketIO = require("socket.io"),
+  io = socketIO(server);
 
 var users = {};
 
 //static files
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 //websocket server
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  console.log(users);
+  socket.on("inputMessage", (message) => {
+    console.log(message);
+    io.emit("chatMessage", message);
+  });
+  socket.on("name", (name) => {
+    users[socket.id] = name;
+    io.emit("users", users);
 
-    console.log(users)
-    socket.on('inputMessage', (message) => {
-        io.emit('chatMessage', message)
-    })
-    socket.on('name', (name) => {
-        users[socket.id] = name;
-        io.emit('users', users)
-
-        socket.on('disconnecting', () => {
-            delete users[socket.id];
-            io.emit('users', users)
-        })
-
-    })
-})
+    socket.on("disconnecting", () => {
+      delete users[socket.id];
+      io.emit("users", users);
+    });
+  });
+});
 
 server.listen(3000, () => {
-    console.log('server is running on port 3000')
-})
+  console.log("server is running on port 3000");
+});
